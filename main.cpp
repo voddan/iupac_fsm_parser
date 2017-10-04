4ac68b902b328e6ee495f29ac05a099ab073c810
voddan <dgvodopyan@gmail.com>
2017-10-04 16:28:55 +0300

    Collected all possible values of chemical name parts

--- main.cpp
+++ main.cpp
@@ -2,35 +2,60 @@
 #include <tinyxml.h>
 
 #include <memory>
+#include <vector>
 #include <base_cpp/non_copyable.h>
 
-class A// : public indigo::NonCopyable
-{
-public:
-    A(int x, double y) : x(x), y(y) {};
 
-    int x;
-    double y;
-};
-
-
-A * create_new_A_ptr() {
-    return new A(1, 2.0);
-}
-
-void create_new_A_write(A& res) {
-    new A(1, 2.0);
-}
-
-
+using std::string;
+using std::vector;
+using std::move;
+using std::cout;
+using std::endl;
 
 int main() {
-    TiXmlDocument doc("C:\\Users\\Daniil_Vodopian\\Documents\\Indigo\\iupac-fsm-parser\\grammar\\alkanes.xml");
-    doc.LoadFile();
-    fprintf(stdout, "hello");
-    doc.Print(stdout, 0);
+    string grammarPath ("C:\\Users\\Daniil_Vodopian\\Documents\\Indigo\\iupac-fsm-parser\\grammar\\");
+    
+    TiXmlDocument indexFile((grammarPath + "index.xml").c_str());
+    indexFile.LoadFile();
 
-  
+    TiXmlNode * tokenFiles = indexFile.FirstChild("tokenFiles");
+    TiXmlNode * tokenFile = tokenFiles->FirstChild("tokenFile");
+
+    vector<string> xmlFileNames;
+    while(tokenFile) {
+        TiXmlNode * value = tokenFile->FirstChild();
+        xmlFileNames.push_back(value->ValueTStr());
+        tokenFile = tokenFiles->IterateChildren("tokenFile", tokenFile);
+    }
+
+    for(auto& fileName : xmlFileNames) {
+        cout << fileName << endl;
+        TiXmlDocument tokenListsFile((grammarPath + fileName).c_str());
+        tokenListsFile.LoadFile();
+
+        TiXmlNode * tokenLists = tokenListsFile.FirstChild("tokenLists");
+        TiXmlNode * tokenList = (tokenLists) ?
+                                tokenLists->FirstChild("tokenList") :
+                                tokenListsFile.FirstChild("tokenList");
+        
+        while(tokenList) {
+            TiXmlNode * token = tokenList->FirstChild("token");
+            while(token) {
+                TiXmlNode * value = token->FirstChild();
+
+                if(value) {
+                    cout << "\t" << *value << endl;
+                } else {
+                    cout << endl;
+                }
+
+                token = tokenList->IterateChildren("token", token);
+            }
+
+            tokenList = (tokenLists) ? tokenLists->IterateChildren("tokenList", tokenList) : nullptr;
+        }
+    }
+
 
 
     return 0;
