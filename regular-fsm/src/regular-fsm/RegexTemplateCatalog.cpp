b486a88ceeaa804922806c08bff40c27e7800cd7
voddan <dgvodopyan@gmail.com>
2017-10-09 16:54:45 +0300

    Extracted splitting a string

--- regular-fsm/src/regular-fsm/RegexTemplateCatalog.cpp
+++ regular-fsm/src/regular-fsm/RegexTemplateCatalog.cpp
@@ -12,6 +12,7 @@
 
 using std::move;
 using std::endl;
+vector<TextPosition> split(char delimiter, TextPosition position);
 
 RegexTemplateCatalog::RegexTemplateCatalog() { }
 
@@ -26,30 +27,15 @@ void RegexTemplateCatalog::addRegexTemplate(string nodeName, string regexTemplat
 }
 
 void RegexTemplateCatalog::addTokenString(string nodeName, string tokenString) {
+    const vector<TextPosition> & branches = split('|', TextPosition(tokenString, 0));
+
     std::unique_ptr<RegexSyntaxTreeNode> root;
-
-    std::regex firstRegex("([^|]+)\\|?");
-    std::smatch firstMatches;
-
-    if(std::regex_search(tokenString, firstMatches, firstRegex)) {
-        auto match = firstMatches[1];
-        RegexSyntaxTreeNode node = fromTrivialString(TextPosition(match, 0));
-        root.reset(new RegexSyntaxTreeNode(move(node)));
-    } else {
-        root.reset(new NaN(0));
-    }
-
-    std::regex regex("\\|([^|]+)");
-    std::smatch matches;
-
-    string::const_iterator begin = tokenString.begin();
-    const string::const_iterator end = tokenString.end();
-
-    while(std::regex_search(begin, end, matches, regex)) {
-        auto match = matches[1];
-        RegexSyntaxTreeNode node = fromTrivialString(TextPosition(match, 0));
-        root.reset(new Combination(move(*root.release()), move(node)));
-        begin = match.second;  // shifts the search to the next match
+    for(auto& branch : branches) {
+        RegexSyntaxTreeNode node = fromTrivialString(branch);
+        if(root)
+            root.reset(new Combination(move(*root.release()), move(node)));
+        else
+            root.reset(new RegexSyntaxTreeNode(move(node)));
     }
 
     tokenStrings[nodeName] = move(tokenString);
@@ -112,4 +98,32 @@ string RegexTemplateCatalog::prettyPrint() {
     }
 
     return str.str();
+}
+
+vector<TextPosition> split(char delimiter, TextPosition position) {
+    string D(1, delimiter);
+    vector<TextPosition> results;
+
+    std::regex firstRegex("([^" + D + "]*)[" + D + "]?");
+    std::smatch firstMatches;
+
+    if(std::regex_search(position.text, firstMatches, firstRegex)) {
+        auto match = firstMatches[1];
+        results.emplace_back(match, 0);
+    }
+
+    std::regex regex("[" + D + "]([^" + D +  "]*)");
+    std::smatch matches;
+
+    const string::const_iterator begin = position.text.begin();
+    const string::const_iterator end = position.text.end();
+    auto iter = begin;
+
+    while(std::regex_search(iter, end, matches, regex)) {
+        auto match = matches[1];
+        results.emplace_back(match, match.first - begin);
+        iter = match.second;  // shifts the search to the next match
+    }
+
+    return results;
 }
